8b9e54360b94abb546cb71f2001dc0b7bce7e26c
Elias-Mascarenhas <128411623+Elias-Mascarenhas@users.noreply.github.com>
2024-05-11 10:08:55 -0300

    Update prototipo.py

--- prototipo.py
+++ prototipo.py
@@ -3,14 +3,61 @@ class OrderSystem:
         self.profile_created = False
         self.order = {}
 
-    def create_profile(self):
-        print("Criação de perfil, com base em restrições/alimentares e preferências")
-        response = input("Você deseja criar um perfil? (SIM/NAO): ")
-        if response.upper() == "SIM":
-            self.profile_created = True
-            print("Perfil criado com sucesso!")
+    usuarios = {}
+
+def cadastrar_usuario(cpf, nome, numero_telefone, genero, idade, observacoes):
+    if cpf not in usuarios:
+        usuarios[cpf] = {"CPF": cpf, "Nome": nome, "Numero": numero_telefone, "Gênero": genero, "Idade": idade, "Observações": observacoes}
+        print("Usuário cadastrado com sucesso!")
+    else:
+        print("CPF já cadastrado!")
+
+def login(cpf):
+    if cpf in usuarios and usuarios[cpf] == cpf:
+        print("Login bem sucedido! Bem-vindo,", usuarios["cpf"])
+    else:
+        print("E-mail ou senha incorretos!")
+
+def entrar_como_visitante():
+    print("Início de atendimento!")
+
+
+def main():
+    while True:
+        print("1. Cadastrar")
+        print("2. Login")
+        print("3. Entrar como visitante")
+        print("4. Sair")
+        choice = input("Escolha uma opção: ")
+
+        if choice == "1":
+            cpf = float(input("Digite seu CPF: "))
+            nome = input("Digite seu Nome: ")
+            numero_telefone = float(input("Digite seu numero de telefone: "))
+            genero = input("Digite seu gênero: ")
+            idade = int(input("Digite sua idade: "))
+            observacoes = input("Se voce tiver coisas que nao pode comer: ")
+            cadastrar_usuario(nome, cpf, numero_telefone, genero, idade, observacoes)
+
+        elif choice == "2":
+            cpf = float(input("Digite seu CPF: "))
+            login(cpf)
+            break
+
+        elif choice == "3":
+            entrar_como_visitante()
+            print("Você entrou como visitante!")
+            break
+
+        elif choice == "4":
+            print("Saindo...")
+            break
+
         else:
-            print("Perfil pré-definido")
+            print("Opção inválida!")
+
+if __name__ == "__main__":
+    main()
 
     def show_menu(self, menu_type):
         print(f"Menu de {menu_type}:")
